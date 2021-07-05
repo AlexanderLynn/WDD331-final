f3ec2fbe9742011b0ef3c8e1bcef578af511cc81
AlexanderLynn <30274747+AlexanderLynn@users.noreply.github.com>
2021-07-05 16:23:04 -0600

    Initial Draggable Function

---
+++ Files/javascript/utilities/draggable.js
@@ -0,0 +1,76 @@
+let deskList = ["0", "1", "2", "3"];
+for (i = 1; i < deskList.length + 1; i++) {
+    console.log("desk"+ i);
+    let div = document.createElement("div");
+    let deskContainer = document.getElementById("deskContainer");
+    deskContainer.appendChild(div);
+    div.setAttribute("class", "desk");
+    div.setAttribute("id", "desk"+i);
+    makeDraggable(div);
+}
+function makeDraggable(desk) {
+    console.log(desk);
+    let dragItem = desk;
+    let container = document.querySelector(".desk-layout");
+
+    let active = false;
+    let currentX;
+    let currentY;
+    let initialX;
+    let initialY;
+    let xOffset = 0;
+    let yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    function dragStart(e) {
+      if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+      } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+      
+        e.preventDefault();
+      
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    }
+}
